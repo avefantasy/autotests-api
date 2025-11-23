bed32c106a29467cb659704da65e6b95b2f687f2
avefantasy <artemkor111@yandex.ru>
2025-11-23 18:37:38 +0300

    Parametrize

--- fixtures/users.py
+++ fixtures/users.py
@@ -1,7 +1,6 @@
 import pytest
 from pydantic import BaseModel, EmailStr
 
-from clients.authentication.authentication_client import AuthenticationClient, get_authentication_client
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import get_private_users_client, PrivateUsersClient
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
@@ -26,11 +25,6 @@ class UserFixture(BaseModel):
 
 
 @pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
-
-
-@pytest.fixture
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
 
@@ -45,5 +39,3 @@ def function_user(public_users_client: PublicUsersClient) -> UserFixture:
     request = CreateUserRequestSchema()
     response = public_users_client.create_user(request)
     return UserFixture(request=request, response=response)
-
-
