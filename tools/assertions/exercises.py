e5ade17af536f37f1c5967c230a044c8be7dd47d
avefantasy <artemkor@yandex.ru>
2025-12-05 13:27:02 +0300

    Adding Allure Steps

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,3 +1,5 @@
+import allure
+
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_schema import ExerciseSchema, CreateExerciseRequestSchema, \
     CreateExerciseResponseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema, \
@@ -6,6 +8,7 @@ from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.errors import assert_internal_error_response
 
 
+@allure.step("Check create exercise response")
 def assert_create_exercise_response(
         request: CreateExerciseRequestSchema,
         response: CreateExerciseResponseSchema
@@ -26,15 +29,16 @@ def assert_create_exercise_response(
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
 
 
+@allure.step("Check update exercise response")
 def assert_update_exercise_response(
         request: UpdateExerciseRequestSchema,
         response: UpdateExerciseResponseSchema
 ):
     """
-    Проверяет, что ответ на обновление задания соответствует запросу.
+    Проверяет, что ответ на обновление задания соответствует данным из запроса.
 
     :param request: Исходный запрос на обновление задания.
-    :param response: Ответ API с данными задания.
+    :param response: Ответ API с обновленными данными задания.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
     assert_equal(response.exercise.title, request.title, "title")
@@ -45,6 +49,7 @@ def assert_update_exercise_response(
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
 
 
+@allure.step("Check exercise")
 def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
     """
     Проверяет, что фактические данные задания соответствуют ожидаемым.
@@ -63,6 +68,7 @@ def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
     assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
 
 
+@allure.step("Check get exercise response")
 def assert_get_exercise_response(
         get_exercise_response: GetExerciseResponseSchema,
         create_exercise_response: CreateExerciseResponseSchema
@@ -77,6 +83,7 @@ def assert_get_exercise_response(
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
 
 
+@allure.step("Check get exercises response")
 def assert_get_exercises_response(
         get_exercises_response: GetExercisesResponseSchema,
         create_exercise_responses: list[CreateExerciseResponseSchema]
@@ -94,6 +101,7 @@ def assert_get_exercises_response(
         assert_exercise(get_exercises_response.exercises[index], create_exercise_response.exercise)
 
 
+@allure.step("Check exercise not found response")
 def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
     """
     Функция для проверки ошибки, если задание не найдено на сервере.
--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -1,9 +1,11 @@
+import allure
+
 from clients.errors_schema import ValidationErrorResponseSchema, ValidationErrorSchema, InternalErrorResponseSchema
 from clients.files.files_schema import CreateFileResponseSchema, CreateFileRequestSchema, GetFileResponseSchema, FileSchema
 from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_validation_error_response, assert_internal_error_response
 
-
+@allure.step("Check create file response")
 def assert_create_file_response(request: CreateFileRequestSchema, response: CreateFileResponseSchema):
     """
     Проверяет, что ответ на создание файла соответствует запросу.
@@ -18,7 +20,35 @@ def assert_create_file_response(request: CreateFileRequestSchema, response: Crea
     assert_equal(response.file.filename, request.filename, "filename")
     assert_equal(response.file.directory, request.directory, "directory")
 
+@allure.step("Check file")
+def assert_file(actual: FileSchema, expected: FileSchema):
+    """
+    Проверяет, что фактические данные файла соответствуют ожидаемым.
 
+    :param actual: Фактические данные файла.
+    :param expected: Ожидаемые данные файла.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.url, expected.url, "url")
+    assert_equal(actual.filename, expected.filename, "filename")
+    assert_equal(actual.directory, expected.directory, "directory")
+
+@allure.step("Check get file response")
+def assert_get_file_response(
+        get_file_response: GetFileResponseSchema,
+        create_file_response: CreateFileResponseSchema
+):
+    """
+    Проверяет, что ответ на получение файла соответствует ответу на его создание.
+
+    :param get_file_response: Ответ API при запросе данных файла.
+    :param create_file_response: Ответ API при создании файла.
+    :raises AssertionError: Если данные файла не совпадают.
+    """
+    assert_file(get_file_response.file, create_file_response.file)
+
+@allure.step("Check create file with empty filename response")
 def assert_create_file_with_empty_filename_response(actual: ValidationErrorResponseSchema):
     """
     Проверяет, что ответ на создание файла с пустым именем файла соответствует ожидаемой валидационной ошибке.
@@ -39,7 +69,7 @@ def assert_create_file_with_empty_filename_response(actual: ValidationErrorRespo
     )
     assert_validation_error_response(actual, expected)
 
-
+@allure.step("Check create file with empty directory response")
 def assert_create_file_with_empty_directory_response(actual: ValidationErrorResponseSchema):
     """
     Проверяет, что ответ на создание файла с пустым значением директории соответствует ожидаемой валидационной ошибке.
@@ -60,7 +90,7 @@ def assert_create_file_with_empty_directory_response(actual: ValidationErrorResp
     )
     assert_validation_error_response(actual, expected)
 
-
+@allure.step("Check file not found response")
 def assert_file_not_found_response(actual: InternalErrorResponseSchema):
     """
     Функция для проверки ошибки, если файл не найден на сервере.
@@ -71,7 +101,7 @@ def assert_file_not_found_response(actual: InternalErrorResponseSchema):
     expected = InternalErrorResponseSchema(details="File not found")
     assert_internal_error_response(actual, expected)
 
-
+@allure.step("Check get file with incorrect file id response")
 def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorResponseSchema):
     """
     Проверяет, что ответ API на запрос файла с некорректным file_id
@@ -97,32 +127,3 @@ def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorRespo
         ]
     )
     assert_validation_error_response(actual, expected)
-
-
-def assert_file(actual: FileSchema, expected: FileSchema):
-    """
-    Проверяет, что фактические данные файла соответствуют ожидаемым.
-
-    :param actual: Фактические данные файла.
-    :param expected: Ожидаемые данные файла.
-    :raises AssertionError: Если хотя бы одно поле не совпадает.
-    """
-    assert_equal(actual.id, expected.id, "id")
-    assert_equal(actual.url, expected.url, "url")
-    assert_equal(actual.filename, expected.filename, "filename")
-    assert_equal(actual.directory, expected.directory, "directory")
-
-
-def assert_get_file_response(
-        get_file_response: GetFileResponseSchema,
-        create_file_response: CreateFileResponseSchema
-):
-    """
-    Проверяет, что ответ на получение файла соответствует ответу на его создание.
-
-    :param get_file_response: Ответ API при запросе данных файла.
-    :param create_file_response: Ответ API при создании файла.
-    :raises AssertionError: Если данные файла не совпадают.
-    """
-    assert_file(get_file_response.file, create_file_response.file)
-
